844307801b04160f756edf5bd60bf0c02da2ef8e
Your Name <sabina.daria3@yahoo.com>
2020-11-06 08:19:44 +0200

    lab3

--- lab3/Main.py
+++ lab3/Main.py
@@ -1,8 +1,43 @@
 from SyT import SymbolTable
+from Scanner import *
+from PIF import *
 
 if __name__ == "__main__":
-    st = SymbolTable()
-    st.add("m")
-    st.add("l")
-    assert (st.add("a")==2)
-    print(st)
+    fileName = "token.in"
+    scanner = Scanner()
+
+
+    with open(fileName,"r") as file:
+        for line in file:
+            print([token for token in scanner.getTokens(line)])
+
+    symbolTable = SymbolTable()
+    pif = ProgramInternalForm()
+    with open(fileName,"r")as file:
+        lineNumber =0
+        for line in file:
+            lineNumber += 1
+            line = line.replace('\n','')
+            for token in scanner.getTokens(line):
+                if token == '' or token =='\n' or token == ' ':
+                    pass
+                elif scanner.verifySeparator(token) or scanner.verifyOperator(token) or scanner.verifyReservedWords(token):
+                    pif.add(scanner.codification[token],-1)
+                elif scanner.verifyConstant(token):
+                    id = symbolTable.add(token)
+                    pif.add(scanner.codification['constant'], id)
+                elif scanner.verifyIdentifier(token):
+                    id = symbolTable.add(token)
+                    pif.add(scanner.codification['identifier'],id)
+                else:
+                    raise Exception('Unknown token ' + token + ' at line ' + str(lineNumber))
+    outPif = open("pif.txt", "w")
+    outSt = open("St.txt", "w")
+    outCodification = open("codification.txt", "w")
+    outSt.write(str(symbolTable))
+    outPif.write(str(pif))
+    s = ""
+    for x, y in scanner.codification.items():
+        s += (str(x) + " " + str(y) + '\n')
+    outCodification.write(s)
+
